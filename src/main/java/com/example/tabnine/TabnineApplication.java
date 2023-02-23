dfb7ca82348332cb9063c6253d4f70ad38a2d467
小霉 <h16619816762@163.com>
2023-02-23 22:08:09 +0800

    cuihan测试

---
+++ src/main/java/com/example/tabnine/TabnineApplication.java
@@ -0,0 +1,13 @@
+package com.example.tabnine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabnineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TabnineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tabnine/TabnineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tabnine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TabnineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
